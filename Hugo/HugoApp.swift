c53de0d8f8c167df56b27886e553ff95b9baac24
Sebastian Ordell Nielsen <s.ordellnielsen@gmail.com>
2025-10-05 01:47:39 +0200

    Initial Commit

---
+++ Hugo/HugoApp.swift
@@ -0,0 +1,32 @@
+//
+//  HugoApp.swift
+//  Hugo
+//
+//  Created by Sebastian Nielsen on 05/10/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HugoApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
