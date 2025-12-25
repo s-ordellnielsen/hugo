cd74b350f02fa40b544ae695a27badc180ce903d
Sebastian Ordell Nielsen <s.ordellnielsen@gmail.com>
2025-12-25 15:44:12 +0100

    Removed any references to submitted reports

--- Hugo/Screens/Report/ReportView.swift
+++ Hugo/Screens/Report/ReportView.swift
@@ -38,57 +38,25 @@ struct ReportView: View {
                     MonthlyReportListView()
                         .padding()
                 }
-
-                VStack(alignment: .leading) {
-                    Text("report.submitted.title")
-                        .padding(.horizontal)
-                        .font(.callout)
-                        .fontWeight(.semibold)
-                        .fontDesign(.rounded)
-                        .foregroundStyle(.secondary)
-                    NavigationLink(destination: Text("reports.all")) {
-                        Label {
-                            HStack {
-                                Text("reports.all")
-                                Spacer()
-                                Image(systemName: "chevron.right")
-                                    .font(.caption)
-                                    .foregroundStyle(.tertiary)
-                                    .fontWeight(.semibold)
-                            }
-                        } icon: {
-                            Image(systemName: "list.bullet")
-                                .symbolRenderingMode(.hierarchical)
-                        }
-                    }
-                    .padding()
-                    .frame(maxWidth: .infinity, alignment: .leading)
-                    .background(Color(.secondarySystemGroupedBackground))
-                    .cornerRadius(24)
-                    .labelReservedIconWidth(24)
-                    .tint(.primary)
-                }
-                .padding()
-                .frame(maxWidth: .infinity, alignment: .leading)
             }
             .frame(maxWidth: .infinity)
             .background(Color(.systemGroupedBackground))
             .navigationTitle("reports.title")
             .navigationBarTitleDisplayMode(.automatic)
-            .toolbar {
-                ToolbarItem {
-                    Menu {
-                        Button("report.add.report", systemImage: "plus") {
-                            addReportSheetIsPresented =  true
-                        }
-                    } label: {
-                        Label("report.add.more", systemImage: "plus")
-                    }
-                }
-            }
-            .sheet(isPresented: $addReportSheetIsPresented) {
-                AddReportSheet()
-            }
+//            .toolbar {
+//                ToolbarItem {
+//                    Menu {
+//                        Button("report.add.report", systemImage: "plus") {
+//                            addReportSheetIsPresented =  true
+//                        }
+//                    } label: {
+//                        Label("report.add.more", systemImage: "plus")
+//                    }
+//                }
+//            }
+//            .sheet(isPresented: $addReportSheetIsPresented) {
+//                AddReportSheet()
+//            }
         }
     }
 }
