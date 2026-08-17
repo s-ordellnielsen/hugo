b8fe1702a7d8cd57c0a60b1f242c4accee6bf9fd
Sebastian Ordell Nielsen <s@ordellnielsen.com>
2026-08-17 06:47:11 +0200

    SLN-215: Empty state for the Overview tab

--- Hugo/Features/Entries/EntryListView.swift
+++ Hugo/Features/Entries/EntryListView.swift
@@ -4,18 +4,39 @@ struct EntryListView: View {
     var entries: [Entry]
 
     @State var selectedEntry: Entry? = nil
+	@State var showAddEntrySheet: Bool = false
 
     var body: some View {
         LazyVStack(spacing: 12) {
-            ForEach(entries) { entry in
-                EntryRow(entry: entry, selectedEntry: $selectedEntry)
-                    .transition(.move(edge: .top).combined(with: .opacity))
-            }
+			if entries.isEmpty {
+				ContentUnavailableView {
+					Label {
+						Text("overview.entries.empty")
+							.fontDesign(.serif)
+					} icon: {
+						Image(systemName: "text.page.badge.magnifyingglass")
+					}
+				} description: {
+					Text("overview.entries.empty.description")
+				} actions: {
+					Button("overview.entries.empty.actions.add") {
+						showAddEntrySheet.toggle()
+					}
+				}
+			} else {
+				ForEach(entries) { entry in
+					EntryRow(entry: entry, selectedEntry: $selectedEntry)
+						.transition(.move(edge: .top).combined(with: .opacity))
+				}
+			}
         }
         .motion(Motion.presence, value: entries.count)
         .sheet(item: $selectedEntry) { entry in
             EntryDetailView(entry: entry)
                 .presentationDetents([.medium])
         }
+		.sheet(isPresented: $showAddEntrySheet) {
+			AddEntryView()
+		}
     }
 }
